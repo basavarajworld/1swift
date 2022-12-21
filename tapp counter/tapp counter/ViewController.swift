0d501342298a907db64ccf7a4afce96ba7998426
Basavaraj N M <95290162+basavarajworld@users.noreply.github.com>
2022-12-21 15:50:54 +0530

    Add files via upload

---
+++ tapp counter/tapp counter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tapp counter
+//
+//  Created by APPLE on 30/09/22.
+//
+
+import UIKit
+
+class TAPCOUNTERVC: UIViewController {
+    @IBOutlet weak var COUNTLABEL: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
