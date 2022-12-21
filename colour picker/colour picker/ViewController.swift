b6b096fe9b79e2a90b6958a20a15a98ab3ca05c5
Basavaraj N M <95290162+basavarajworld@users.noreply.github.com>
2022-12-21 15:45:38 +0530

    Add files via upload

---
+++ colour picker/colour picker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  colour picker
+//
+//  Created by APPLE on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colourview: UIView!
+    override func viewDidLoad() {
+        func `switch`(_ sender: Any) {
+        }
+        super.viewDidLoad()
+        
+
+    }
+}
+
